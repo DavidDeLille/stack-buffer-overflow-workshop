8b4dd9b412b77e94d679e2e8b2564516fc4ff1b9
David De Lille <DavidDeLille@users.noreply.github.com>
2019-08-31 21:49:25 +1000

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -10,3 +10,8 @@ sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
+
+# Pwntools
+sudo apt-get install python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential
+pip2 install --upgrade pip
+pip2 install --upgrade pwntools
