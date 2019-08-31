c7b507e935d40d6cafa1f7f05ba5e7f022035a69
David De Lille <DavidDeLille@users.noreply.github.com>
2019-09-01 06:53:10 +1000

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -5,7 +5,7 @@ cd pwndbg
 ./setup.sh
 
 # Sublime Text 3
-get -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
