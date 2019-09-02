92b9c2598b4523ba6a029e59d0cbc4cac6e9db91
David De Lille <DavidDeLille@users.noreply.github.com>
2019-09-03 06:51:37 +1000

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,3 +1,10 @@
+# Sublime Text 3 (this will also run apt-get update)
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
 # 32-bit support
 apt-get install gcc-multilib
 
@@ -6,13 +13,6 @@ cd /opt
 git clone https://github.com/pwndbg/pwndbg
 cd pwndbg
 ./setup.sh
-
-# Sublime Text 3
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install sublime-text
 
 # Pwntools
 sudo apt-get install python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential -y
